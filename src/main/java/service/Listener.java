aa856b8add924fed1d009f43eb67ecb71f20907a
pEvgeniy <PopEvgeniyV@yandex.ru>
2023-03-04 18:43:10 +0300

    refactor: log

--- src/main/java/service/Listener.java
+++ src/main/java/service/Listener.java
@@ -4,18 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.pcap4j.core.PacketListener;
 import org.pcap4j.packet.Packet;
 import service.managers.InMemoryHistoryManager;
-import service.parser.MsgParser;
+import service.parser.SampledValuesParser;
 
 @Slf4j
 public class Listener implements PacketListener {
-    MsgParser msgParser = new MsgParser();
+    SampledValuesParser msgParser = new SampledValuesParser();
     InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
 
     @Override
     public void gotPacket(Packet packet) {
-        String receivedMsg = msgParser.parse(packet.getRawData());
+        byte[] rawData = packet.getRawData();
+        String receivedMsg = msgParser.parse(rawData);
         Packet payload = packet.getPayload();
-        log.debug("received msg = {}\npayload = {}", receivedMsg, payload);
+        log.info("received msg = {}\n" +
+                "payload = {}", receivedMsg, payload);
         historyManager.add(receivedMsg);
     }
 }
